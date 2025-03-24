71483cd4a514fedf34d8108a99f090f1acc9fe8b
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-03-24 06:19:54 -0300

    Implementation of class "Address"

--- src/main/java/br/com/fcamara/estacionamento/model/Address.java
+++ src/main/java/br/com/fcamara/estacionamento/model/Address.java
@@ -1,4 +1,29 @@
 package br.com.fcamara.estacionamento.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Embeddable
 public class Address {
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    private String street;
+
+    private String number;
+
+    private String neighborhood;
+
+    private String city;
+
+    private String state;
+
+    private String country;
 }

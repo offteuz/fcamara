aed761c64682fdd03d11933fd200ee378332b3d0
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-03-24 06:20:21 -0300

    Implementation of class "Vehicle"

--- src/main/java/br/com/fcamara/estacionamento/model/Vehicle.java
+++ src/main/java/br/com/fcamara/estacionamento/model/Vehicle.java
@@ -1,4 +1,34 @@
 package br.com.fcamara.estacionamento.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity(name = "VEHICLE")
 public class Vehicle {
+
+    @Id
+    @SequenceGenerator(name = "seq_vehicle", sequenceName = "seq_vehicle", allocationSize = 1)
+    @GeneratedValue(generator = "seq_vehicle", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String mark;
+
+    private String model;
+
+    private String color;
+
+    private String plate;
+
+    private String type;
+
+    @ManyToOne
+    @JoinColumn(name = "establishment_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Establishment establishment;
 }

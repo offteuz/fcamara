3010161a2d9ddfcb45ddfb7322b383b528029389
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-03-24 06:20:06 -0300

    Implementation of class "Establishment"

--- src/main/java/br/com/fcamara/estacionamento/model/Establishment.java
+++ src/main/java/br/com/fcamara/estacionamento/model/Establishment.java
@@ -1,4 +1,38 @@
 package br.com.fcamara.estacionamento.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity(name = "ESTABLISHMENT")
 public class Establishment {
+
+    @Id
+    @SequenceGenerator(name = "seq_establishment", sequenceName = "seq_establishment", allocationSize = 1)
+    @GeneratedValue(generator = "seq_establishment", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+
+    private String cnpj;
+
+    @Embedded
+    private Address address;
+
+    private String telephone;
+
+    @Column(name = "motorcycle_vacancy")
+    private int motorcycleVacancy;
+
+    @Column(name = "car_vacancy")
+    private int carVacancy;
+
+    @OneToMany(mappedBy = "establishment")
+    private Set<Vehicle> vehicles;
 }
